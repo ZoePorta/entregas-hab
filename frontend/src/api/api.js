d9aa23e870ef121468f61f217dc9a280a45bd37f
ZoePorta <zoeportagarcia@gmail.com>
2020-05-30 20:57:36 +0200

    Entrega 1 frontend - Rick y Morty API

---
+++ frontend/src/api/api.js
@@ -0,0 +1,18 @@
+// INDICADNDO EL USO DE AXIOS PARA LA API
+const axios = require("axios").default;
+
+//BASE URL DE LA API
+const apiUrl = "https://rickandmortyapi.com/api";
+
+//FUNCION PARA COGER TODOS LOS CHARS
+function getAll() {
+  return axios.get(`${apiUrl}/character`);
+}
+function getChar(id) {
+  return axios.get(`${apiUrl}/character/` + id);
+}
+
+export default {
+  getAll,
+  getChar,
+};
